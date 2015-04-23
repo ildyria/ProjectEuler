4274842b828ead431d1bb69847a747f08fa8afec
ildyria <beviguier@gmail.com>
2015-04-23 15:21:15 +0200

    problem 29 wip

---
+++ problem/29.cpp
@@ -0,0 +1,7 @@
+#include "29.h"
+
+void Problem_29::run()
+{
+	printf("to be solved...\n");
+}
+
---
+++ problem/29.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <vector>
+#include "../includes/problem.h"
+
+class Problem_29 : public Problem
+{
+public:
+	Problem_29():Problem() {};
+	~Problem_29() {};
+	void run();
+};
+
